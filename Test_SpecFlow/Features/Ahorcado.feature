30f3b44a375fd78b78f06c01b9d39f3be3914b75
lisandroferroni <lisandro.ferroni@gmail.com>
2020-12-08 18:50:35 -0300

    Tests de Specflow

---
+++ Test_SpecFlow/Features/Ahorcado.feature
@@ -0,0 +1,11 @@
+﻿Feature: Ahorcado
+
+Scenario: Arriesgar palabra completa correcta
+	Given la palabra a adivinar es palabra
+	When se arriesga la palabra palabra
+	Then el resultado deberia ser True
+
+Scenario: Arriesgar palabra completa incorrecta
+	Given la palabra a adivinar es palabra
+	When se arriesga la palabra incorrecta
+	Then el resultado deberia ser False
---
+++ Test_SpecFlow/Steps/AhorcadoStepDefinitions.cs
@@ -0,0 +1,43 @@
+﻿using TechTalk.SpecFlow;
+using Ahorcado.Controllers;
+using Ahorcado.Models;
+using Newtonsoft.Json;
+using Ahorcado.Util;
+using FluentAssertions;
+
+namespace Test_SpecFlow.Steps
+{
+    [Binding]
+    public sealed class AhorcadoStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private Ahorcado.Ahorcado _ahorcado { get; set; }
+        private readonly AhorcadoController _ahorcadoControlador = new AhorcadoController();
+        private Result _resultado { get; set; }
+
+        public AhorcadoStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("la palabra a adivinar es (.*)")]
+        public void DadaLaPrimerPalabra(string palabra)
+        {
+            PalabraInput palabraInput = new PalabraInput() { Palabra = palabra};
+            _ahorcadoControlador.InicializarAhorcadoMultijugador(palabraInput);
+        }
+
+        [When("se arriesga la palabra (.*)")]
+        public void CuandoSeArriesgaLaPalabra(string palabraArriesgada)
+        {
+            PalabraInput palabraInput = new PalabraInput() { Palabra = palabraArriesgada };
+            _resultado = JsonConvert.DeserializeObject<Result> (_ahorcadoControlador.ArriesgaPalabra(palabraInput));
+        }
+
+        [Then("el resultado deberia ser (.*)")]
+        public void ElResultadoDeberiaSer(string resultado)
+        {
+            _resultado.Value.Should().Be(resultado);
+        }
+    }
+}
